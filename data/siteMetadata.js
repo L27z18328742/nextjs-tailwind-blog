0c060a2c21b2de3f7833755d876b63c31d1c629f
yinbanxing <2718328742@qq.com>
2025-04-25 12:11:08 +0800

    小乐的笔记修改2

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 const siteMetadata = {
-  title: '狂奔滴小马 | 前端工程师成长部落阁',
-  author: '狂奔滴小马',
+  title: '小乐的笔记 | 全栈工程师成长部落阁',
+  author: '小乐的笔记',
   headerTitle: '小乐的笔记',
   description: '分享 JavaScript 以及热门框架，记录前端工程师学习成长历程。',
   language: 'en-us',
